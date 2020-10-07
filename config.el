ad9b9ffea295f93619802f7310ed0b2960d998dc
Arseny Amerbaev <amerbaev@group-ib.com>
2020-10-07 23:53:41 +0300

    fix default font name

--- config.el
+++ config.el
@@ -18,9 +18,9 @@
 ;;   presentations or streaming.
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "PragmataPro Mono Liga" :size 17)
-      doom-variable-pitch-font (font-spec :family "Pragmata Pro Mono Liga" :size 13))
+;; font string. You generally only need these two
+(setq doom-font (font-spec :family "PragmataPro Liga" :size 17)
+      doom-variable-pitch-font (font-spec :family "PragmataPro Liga" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
